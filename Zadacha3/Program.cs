3ca0d2555800a0b40c63235d617854cd475027dd
Anton <anton.esev@mail.ru>
2023-06-17 14:48:41 +0300

    sem8

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+using System;
+
+class Program {
+    static void Main() {
+        int[,] matrix1 = {{2, 4}, {2, 2}};
+        int[,] matrix2 = {{3, 4}, {3, 3}};
+
+        int[,] product = MatrixProduct(matrix1, matrix2);
+
+        PrintMatrix(product);
+    }
+
+    static int[,] MatrixProduct(int[,] a, int[,] b) {
+        if (a.GetLength(1) != b.GetLength(0)) {
+            throw new ArgumentException("Матрицы имеют неверный размер.");
+        }
+
+        int[,] product = new int[a.GetLength(0), b.GetLength(1)];
+
+        for (int i = 0; i < a.GetLength(0); i++) {
+            for (int j = 0; j < b.GetLength(1); j++) {
+                int sum = 0;
+                for (int k = 0; k < a.GetLength(1); k++) {
+                    sum += a[i, k] * b[k, j];
+                }
+                product[i, j] = sum;
+            }
+        }
+
+        return product;
+    }
+
+    static void PrintMatrix(int[,] matrix) {
+        for (int i = 0; i < matrix.GetLength(0); i++) {
+            for (int j = 0; j < matrix.GetLength(1); j++) {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Zadacha5/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
+using System;
+class SpiralArray
+{
+    static void Main()
+    {
+        int[,] arr = new int[4, 4];
+
+        int value = 1;
+        int row = 0, col = 0;
+        int lastRow = 3, lastCol = 3;
+
+        while (row <= lastRow && col <= lastCol)
+        {
+            for (int i = col; i <= lastCol; i++)
+            {
+                arr[row, i] = value++;
+            }
+            row++;
+            for (int i = row; i <= lastRow; i++)
+            {
+                arr[i, lastCol] = value++;
+            }
+            lastCol--;
+            if (row <= lastRow)
+            {
+                for (int i = lastCol; i >= col; i--)
+                {
+                    arr[lastRow, i] = value++;
+                }
+                lastRow--;
+            }
+            if (col <= lastCol)
+            {
+                for (int i = lastRow; i >= row; i--)
+                {
+                    arr[i, col] = value++;
+                }
+                col++;
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write($"{arr[i, j]:D2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
